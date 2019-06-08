c1d2ffd00020029346af7007b32e6cb13449c2fd
Chris Lin <chris522229197@gmail.com>
2019-06-08 08:35:11 -0700

    Add simulation results

--- simulate.sh
+++ simulate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-Rscript simulate.R -b 0.005 -m 0.005 -r 0.8 -N 500 -t 50000 -o 7 -v
-Rscript simulate.R -b 0.01 -m 0.005 -r 0.8 -N 500 -t 50000 -o 7 -v
-Rscript simulate.R -b 0.005 -m 0.01 -r 0.8 -N 500 -t 50000 -o 7 -v
+# Rscript simulate.R -b 0.005 -m 0.005 -r 0.8 -N 500 -t 50000 -o 7 -v
+Rscript simulate.R -b 0.5 -m 0.005 -r 0.8 -N 500 -t 50000 -o 7 -v
+Rscript simulate.R -b 0.005 -m 0.5 -r 0.8 -N 500 -t 50000 -o 7 -v
